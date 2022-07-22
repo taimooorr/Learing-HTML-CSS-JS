0c1e0d2ba33ddaf67cd489496b90d8896703b3e5
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-07-23 01:18:41 +0500

    Added Array Practice

--- JsBasic.js
+++ JsBasic.js
@@ -2,4 +2,17 @@ var myname = "Taimoor";
 console.log(myname);
 var myage = "21";
 console.log(typeof(myage));
+
+
 /* Arrays */
+
+var colors = new Array("red", "green", "blue");
+console.log(colors);
+console.log("length of array: " + colors.length);
+console.log(colors[0]);
+var animals = ["dog", "cat", "bird"];
+console.log(animals.length);
+animals.push("elephant");
+console.log(animals);
+animals.pop("dog");
+console.log(animals);
