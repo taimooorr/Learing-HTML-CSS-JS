853830da3894f8a0105b3bb0efe256b13e2dacca
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-07-23 11:51:33 +0500

    Function Declaration and Callling

---
+++ FunctionsInJs.js
@@ -0,0 +1,18 @@
+//Functions
+//1. Function Declaration
+
+function doStuff() {
+    console.log("I'm doing stuff");
+}
+//calling the function
+//doStuff();
+
+//getting input from the user
+// var userInput = prompt("Enter Your Name");
+// console.log(userInput);
+function getSum(num1, num2) {
+    var sum = num1 + num2;
+    return sum;
+}
+var sum = getSum(1, 2);
+console.log(sum);
