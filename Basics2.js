a854445da5dfc99410e5a718f54972c1667b4aef
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-07-23 09:13:48 +0500

    Boolean Operators and IF Else

--- Basics2.js
+++ Basics2.js
@@ -11,4 +11,50 @@ console.log(typeof(isOff));
 
 var green;
 console.log(green);/*output: undefined 0r false beacuse in JavaScript, 
-and 0 is also false*/
+and 0 is also false and Nan  and Null*/
+
+//if else
+if(isOn){
+    console.log("The light is on");
+}
+else{
+    console.log("The light is off");
+}
+
+//boolean operators
+/**
+ * == equal to
+ * != not equal to
+ * > greater than
+ * < less than
+ * >= greater than or equal to
+ * <= less than or equal to
+ * === strict equal to
+ * !== strict not equal to
+ * && and
+ * || or
+ * ! not
+ */
+var value1 = 10;
+var value2 = 20;
+var Sval = "10";
+if(value1 == value2){
+    console.log("value1 is equal to value2");
+}
+else{
+    console.log("value1 is not equal to value2");
+}
+//== is equal to and === is strict equal to
+if(value1 == Sval){
+    console.log("value1 is equal to Sval");// Output: value1 is equal to Sval
+}
+else{
+    console.log("value1 is not equal to Sval");
+}
+
+if(value1===Sval){
+    console.log("value1 is equal to Sval");// Output: value1 is not equal to Sval
+}
+else{
+    console.log("value1 is not equal to Sval");
+}
