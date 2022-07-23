ce5eeb258e346aec968bc4bc95dad54df17ebb4a
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-07-23 11:52:13 +0500

    implemented annonymous function

--- FunctionsInJs.js
+++ FunctionsInJs.js
@@ -16,3 +16,12 @@ function getSum(num1, num2) {
 }
 var sum = getSum(1, 2);
 console.log(sum);
+//annonymous function
+var myFunction = function (num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+console.log(myFunction(1, 2));
