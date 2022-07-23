f649d689026edf753454a73c2fdd3d470c774fce
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-07-23 08:56:24 +0500

    Booleans

---
+++ Basics2.js
@@ -0,0 +1,14 @@
+/* 
+Operators, Booleans & Selection in JavaScript
+*/
+
+//Boolean 
+var isOn = true;
+console.log(typeof(isOn));
+
+var isOff = false;
+console.log(typeof(isOff));
+
+var green;
+console.log(green);/*output: undefined 0r false beacuse in JavaScript, 
+and 0 is also false*/
